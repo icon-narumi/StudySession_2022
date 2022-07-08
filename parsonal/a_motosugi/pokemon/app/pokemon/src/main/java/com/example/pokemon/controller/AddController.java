a92cf96d8099f33c60a95ac0ef3879926fa3505f
DESKTOP-DEPTKV0\t.mogi <t.mogi@iconfor.co.jp>
2022-07-08 10:50:43 +0900

    試しにコメントアウト追加してみました。 ソースは何もしておりません。

--- parsonal/a_motosugi/pokemon/app/pokemon/src/main/java/com/example/pokemon/controller/AddController.java
+++ parsonal/a_motosugi/pokemon/app/pokemon/src/main/java/com/example/pokemon/controller/AddController.java
@@ -56,6 +56,7 @@ public class AddController {
     }
 
     // 手持ち追加したよ
+    //これ何見て作ったんですか？茂木
     @PostMapping(value = "/add/return", params = "add")
     public String addPartner(@Valid @ModelAttribute AddPartnerForm addPartnerForm, BindingResult bindingResult, Model model) {
         
