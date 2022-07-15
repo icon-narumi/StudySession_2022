82b5f60208d14c35c3242efbaa0607ad695f75fd
DESKTOP-DEPTKV0\t.mogi <t.mogi@iconfor.co.jp>
2022-07-15 16:03:00 +0900

    削除方法考え中

--- parsonal/t_mogi/rental2/app/rental2/rental2/rental2/src/main/java/com/example/rental2/controller/CustomerInformationController.java
+++ parsonal/t_mogi/rental2/app/rental2/rental2/rental2/src/main/java/com/example/rental2/controller/CustomerInformationController.java
@@ -1,5 +1,6 @@
 package com.example.rental2.controller;
 
+import org.hibernate.validator.internal.metadata.descriptor.ReturnValueDescriptorImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.rental2.form.CustomerAddForm;
+import com.example.rental2.form.CustomerDeleteForm;
 import com.example.rental2.service.CustomerRegistService;
 
 //顧客情報管理コントローラー
@@ -53,7 +55,7 @@ public class CustomerInformationController {
 
     // 削除初期画面
     @GetMapping("/customerInformation/delete")
-    public String deleteprosece(Model model) {
+    public String deleteExecute(Model model) {
 
         CustomerDeleteForm customerDeleteForm = new CustomerDeleteForm();
 
@@ -61,5 +63,22 @@ public class CustomerInformationController {
         customerDeleteForm.setAgelist(customerRegistService.selectAgeAll());
         customerDeleteForm.setGenderlist(customerRegistService.selectGenderAll());
         customerDeleteForm.setList(customerRegistService.selectByCustomerInformation());
+
+        model.addAttribute("customerDeleteForm", customerDeleteForm);
+        return "customerDelete";
+    }
+
+
+    //削除処理
+    @RequestMapping(value = "/customerInformation/delete", params = "delete", method = RequestMethod.POST)
+    public String deleteProsece(@ModelAttribute CustomerDeleteForm customerDeleteForm, Model model){
+
+        customerDeleteForm.setAgelist(customerRegistService.selectAgeAll());
+        customerDeleteForm.setGenderlist(customerRegistService.selectGenderAll());
+        customerDeleteForm.setList(customerRegistService.selectByCustomerInformation());
+
+
+        model.addAttribute("customerDeleteForm",customerDeleteForm );
+        return "customerInformation";
     }
 }
