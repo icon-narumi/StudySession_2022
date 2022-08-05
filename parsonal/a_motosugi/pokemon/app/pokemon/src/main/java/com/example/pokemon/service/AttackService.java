d16f31d8f50d687d2172279e40f50f8deb6f63c5
DESKTOP-L00OK9T\icon <a.motosugi@iconfor.co.jp>
2022-08-05 19:14:27 +0900

    複合タイプ相性バトル実装

--- parsonal/a_motosugi/pokemon/app/pokemon/src/main/java/com/example/pokemon/service/AttackService.java
+++ parsonal/a_motosugi/pokemon/app/pokemon/src/main/java/com/example/pokemon/service/AttackService.java
@@ -42,21 +42,38 @@ public class AttackService {
         Integer power2 = pokemon2.getPower();
         // タイプ・攻撃タイプ
         String pokemon1Type1 = pokemon1.getType1();
+        String pokemon1Type2 = pokemon1.getType2();
         String pokemon2Type1 = pokemon2.getType1();
+        String pokemon2Type2 = pokemon2.getType2();
         String pokemon1AttackType = pokemon1.getAttackType();
         String pokemon2AttackType = pokemon2.getAttackType();
 
-        // 効果
-        double effect1 = typeCompatibilityService.selectTypeCompatibilityEffect(pokemon1AttackType, pokemon2Type1);
-        double effect2 = typeCompatibilityService.selectTypeCompatibilityEffect(pokemon2AttackType, pokemon1Type1);
+        // 単タイプかもしれないから先にタイプ２に対する効果は１を設定
+        double effect1_2 = 1;
+        double effect2_2 = 1;
+
+        // 効果:こうげき１、まもり２のタイプ１
+        double effect1_1 = typeCompatibilityService.selectTypeCompatibilityEffect(pokemon1AttackType, pokemon2Type1);
+        // もし複合タイプなら　効果:こうげき１、まもり２のタイプ２
+        if(pokemon2Type2 != null) {
+            effect1_2 = typeCompatibilityService.selectTypeCompatibilityEffect(pokemon1AttackType, pokemon2Type2);
+        }
+        // 効果:こうげき２、まもり１のタイプ１
+        double effect2_1 = typeCompatibilityService.selectTypeCompatibilityEffect(pokemon2AttackType, pokemon1Type1);
+        // もし複合タイプなら　効果:こうげき２、まもり１のタイプ２
+        if(pokemon1Type2 != null) {
+            effect2_2 = typeCompatibilityService.selectTypeCompatibilityEffect(pokemon2AttackType, pokemon1Type2);
+        }   
 
         // 計算用
         BigDecimal culStrength1 = new BigDecimal(strength1);
         BigDecimal culStrength2 = new BigDecimal(strength2);
         BigDecimal culPower1 = new BigDecimal(power1);
         BigDecimal culPower2 = new BigDecimal(power2);
-        BigDecimal culEffect1 = new BigDecimal(effect1);
-        BigDecimal culEffect2 = new BigDecimal(effect2);
+        BigDecimal culEffect1_1 = new BigDecimal(effect1_1);
+        BigDecimal culEffect1_2 = new BigDecimal(effect1_2);
+        BigDecimal culEffect2_1 = new BigDecimal(effect2_1);
+        BigDecimal culEffect2_2 = new BigDecimal(effect2_2);
         BigDecimal resultStrength1 = new BigDecimal(strength1);
         BigDecimal resultStrength2 = new BigDecimal(strength2);
 
@@ -64,13 +81,23 @@ public class AttackService {
         while(resultStrength1.compareTo(BigDecimal.ZERO) > 0 && resultStrength2.compareTo(BigDecimal.ZERO) > 0) {
             
             // ポケモン1の体力
-            resultStrength1 = culStrength1.subtract(culPower2.multiply(culEffect2)).setScale(0, RoundingMode.HALF_UP); 
-            System.out.println("１→" + "(つよさ)" + culStrength1 + "- (こうげき)" + culPower2 + " × (効果)" + culEffect2 + " ＝ " + resultStrength1);
-            
+            BigDecimal aaa = resultStrength1;
+            resultStrength1 = resultStrength1.subtract(culPower2.multiply(culEffect2_1).multiply(culEffect2_2)).setScale(0, RoundingMode.HALF_UP); 
+            /*resultStrength1 = culPower2.multiply(culEffect2_1);
+            resultStrength1 = resultStrength1.multiply(culEffect2_2);
+            resultStrength1 = culStrength1.subtract(resultStrength1).setScale(0, RoundingMode.HALF_UP);*/
+            //strength1 = strength1 - power2 * effect2_1 * effect2_2; 
+            System.out.println(pokemon1.getName() + "(つよさ)" + aaa + "- (こうげき)" + culPower2 + 
+                                " × (効果1)" + culEffect2_1 + " × (効果2)" + culEffect2_2 + " ＝ "+ resultStrength1); 
+
             // ポケモン2の体力
-            resultStrength2 = culStrength2.subtract(culPower1.multiply(culEffect1)).setScale(0, RoundingMode.HALF_UP);
-            System.out.println("２→" + "(つよさ)" + culStrength2 + "- (こうげき)" + culPower1 + " × (効果)" + culEffect1 + " ＝ " + resultStrength2);
-            //strength1 = strength1 - power2 * effect2;  
+            BigDecimal bbb = resultStrength2;
+            resultStrength2 = resultStrength2.subtract(culPower1.multiply(culEffect1_1).multiply(culEffect1_2)).setScale(0, RoundingMode.HALF_UP);
+           /* resultStrength2 = culPower1.multiply(culEffect1_1);
+            resultStrength2 = resultStrength2.multiply(culEffect1_2);
+            resultStrength2 = culStrength2.subtract(resultStrength2).setScale(0, RoundingMode.HALF_UP); */
+            System.out.println(pokemon2.getName() + "(つよさ)" + bbb + "- (こうげき)" + culPower1 + 
+                                " × (効果1)" + culEffect1_1  + " × (効果2)" + culEffect1_2 + " ＝ " + resultStrength2);  
             //strength2 = strength2 - power1 * effect1; 
         }
 
